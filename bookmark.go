421f4c668415a0d0dfe9e19d794256f342d1a389
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-01-19 23:39:09 +0900

    add missing file

---
+++ bookmark.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync/atomic"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip04"
+	"github.com/nbd-wtf/go-nostr/nip19"
+	"github.com/nbd-wtf/nostr-sdk"
+)
+
+func doBMList(cCtx *cli.Context) error {
+	n := cCtx.Int("n")
+	j := cCtx.Bool("json")
+	extra := cCtx.Bool("extra")
+
+	cfg := cCtx.App.Metadata["config"].(*Config)
+
+	// get followers
+	followsMap, err := cfg.GetFollows(cCtx.String("a"))
+	if err != nil {
+		return err
+	}
+
+	var sk string
+	var npub string
+	if _, s, err := nip19.Decode(cfg.PrivateKey); err == nil {
+		sk = s.(string)
+	} else {
+		return err
+	}
+	if npub, err = nostr.GetPublicKey(sk); err != nil {
+		return err
+	}
+
+	// get timeline
+	filter := nostr.Filter{
+		Kinds:   []int{nostr.KindCategorizedBookmarksList},
+		Authors: []string{npub},
+		Tags:    nostr.TagMap{"d": []string{"bookmark"}},
+		Limit:   n,
+	}
+
+	be := []string{}
+	evs := cfg.Events(filter)
+	for _, ev := range evs {
+		for _, tag := range ev.Tags {
+			if len(tag) > 1 && tag[0] == "e" {
+				be = append(be, tag[1:]...)
+			}
+		}
+	}
+	filter = nostr.Filter{
+		Kinds: []int{nostr.KindTextNote},
+		IDs:   be,
+	}
+	eevs := cfg.Events(filter)
+	cfg.PrintEvents(eevs, followsMap, j, extra)
+	return nil
+}
+
+func doBMPost(cCtx *cli.Context) error {
+	return errors.New("Not Implemented")
+}
