b6c8c15cb3a6c0725dd5490bb66e8627a2fac418
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-01-28 00:40:31 +0900

    use StringSlice

--- main.go
+++ main.go
@@ -481,7 +481,7 @@ func main() {
 				Name:  "stream",
 				Usage: "show stream",
 				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "author"},
+					&cli.StringSliceFlag{Name: "author"},
 					&cli.IntSliceFlag{Name: "kind", Value: cli.NewIntSlice(nostr.KindTextNote)},
 					&cli.BoolFlag{Name: "follow"},
 					&cli.StringFlag{Name: "pattern"},
